d4c91a15e94cff3a1c5172a059dc36dddade71db
Nikita <nekit.im@yandex.ru>
2023-10-15 16:23:45 +0300

    Start

--- pr/Main.cpp
+++ pr/Main.cpp
@@ -1,35 +1,55 @@
 #define F_CPU 8000000L
- #include <avr/io.h> 
+// Подключение семисегментных индикаторов к AVR. Динамическая индикация
+#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+//---------------------------0-----1-----2-----3-----4-----5-----6-----7-----8------9
+unsigned char SEGMENTE[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
-void port_ini(void){
-	 PORTC=0b00000010; //  1 пин на выход
-	  DDRC=0b00000010; // выход высокого уровня на одном пине
-	  
-	 PORTB=0x00; // 
-	 DDRB=0b00100000; 
-	 
-	
-}
+unsigned char segcounter = 0;
+unsigned int display = 0;
 
-
-void impulse()
+// Обработчик прерывания по переполнению таймера 2
+ISR(TIMER2_OVF_vect)
 {
-	PORTB |= 1<<5;				// импульс вкл (пин 5 в высокий уровень)
-	_delay_ms(600);     // длительность импульса
-	PORTB &= ~(1<<5);			// импульс выкл  (пин 5 в низкий уровень)
+	PORTD = 0xFF; // Гасим все разряды
+	PORTB = (1 << segcounter); // Выбираем следующий разряд
+	
+	switch (segcounter)
+	{
+	case 0:
+		PORTD = ~(SEGMENTE[display % 10000 / 1000]); // Раскладываем число на разряды
+		break;
+	case 1:
+		PORTD = ~(SEGMENTE[display % 1000 / 100]);
+		break;
+	case 2:
+		PORTD = ~(SEGMENTE[display % 100 / 10]);
+		break;
+	case 3:
+		PORTD = ~(SEGMENTE[display % 10]);
+		break;
+	}
+	if (segcounter++ > 2) segcounter = 0;
 }
-
+// Главная функция
 int main(void)
 {
-    /* Replace with your application code */
-	port_ini();
-    while (1) 
-    {
-		if( !(PINC & (1 << 1)) ) //если на первом пине низкий уровень (кнопка прижала контакт к земле) делаем импульс
-			impulse();
-			
-    }
+	DDRB = 0xFF; // Порт B - выход
+	PORTB = 0x00;
+	DDRD = 0xFF; // Порт D - выход
+	PORTD = 0x00;
+	
+	TCCR2B |= (1 << CS21); // Предделитель на 8
+	TIMSK2 |= (1 << TOIE2); // Разрешение прерывания по таймеру 2
+	
+	sei(); // Глобально разрешаем прерывания
+	
+	while (1)
+	{
+		display++; // Увеличиваем счет от 0 до 9999
+		if (display > 9999) display = 0;
+		_delay_ms(10); // Задержка
+	}
 }
